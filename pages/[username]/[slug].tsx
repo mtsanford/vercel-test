b881a50419db73b3855426eec1a0bbec9da660c1
Mark Sanford <marktsanford@gmail.com>
2021-11-17 15:16:11 -0800

    post page w/ ISR

--- pages/[username]/[slug].tsx
+++ pages/[username]/[slug].tsx
@@ -1,8 +1,97 @@
-export default function PostPage({}) {
-    return (
-      <main>
-        <h1>Post page</h1>
-      </main>
-    );
+import { doc, collection, collectionGroup, getDoc, getDocs } from 'firebase/firestore';
+import Link from "next/link";
+
+
+import styles from "../../styles/Post.module.css";
+import PostContent from "../../components/PostContent";
+// import HeartButton from "@components/HeartButton";
+// import AuthCheck from "@components/AuthCheck";
+import Metatags from "../../components/Metatags";
+import { UserContext } from "../../lib/context";
+import { firestore, getUserWithUsername, postToJSON } from "../../lib/firebase";
+
+import { useDocumentData } from "react-firebase-hooks/firestore";
+import { useContext } from "react";
+
+export async function getStaticProps({ params }) {
+  const { username, slug } = params;
+  const userDoc = await getUserWithUsername(username);
+
+  let post;
+  let path;
+
+  if (userDoc) {
+    const postRef = doc(collection(userDoc.ref, "posts"), slug);
+    post = postToJSON(await getDoc(postRef));
+
+    path = postRef.path;
   }
-  
+
+  return {
+    props: { post, path },
+    revalidate: 100,
+  };
+}
+
+export async function getStaticPaths() {
+  // Improve my using Admin SDK to select empty docs
+  const cg = collectionGroup(firestore, "posts")
+  const snapshot = await getDocs(cg);
+
+  const paths = snapshot.docs.map((doc) => {
+    const { slug, username } = doc.data();
+    return {
+      params: { username, slug },
+    };
+  });
+
+  return {
+    // must be in this format:
+    // paths: [
+    //   { params: { username, slug }}
+    // ],
+    paths,
+    fallback: "blocking",
+  };
+}
+
+export default function Post(props) {
+  const postRef = doc(firestore, props.path);
+  const [realtimePost] = useDocumentData(postRef);
+
+  const post = realtimePost || props.post;
+
+  const { user: currentUser } = useContext(UserContext);
+
+  return (
+    <main className={styles.container}>
+      <Metatags title={post.title} description={post.title} />
+
+      <section>
+        <PostContent post={post} />
+      </section>
+
+      <aside className="card">
+        <p>
+          <strong>{post.heartCount || 0} 🤍</strong>
+        </p>
+
+        {/* <AuthCheck
+          fallback={
+            <Link href="/enter">
+              <button>💗 Sign Up</button>
+            </Link>
+          }
+        >
+          <HeartButton postRef={postRef} />
+        </AuthCheck> */}
+
+        {currentUser?.uid === post.uid && (
+          <Link href={`/admin/${post.slug}`}>
+            <button className="btn-blue">Edit Post</button>
+          </Link>
+        )}
+      </aside>
+    </main>
+  );
+}
